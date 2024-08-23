fc3297376175515e7b8fcf2967ee3172ab21b3d3
Christopher Cortes <christopherctes@hotmail.com>
2024-08-23 17:31:59 -0400

    membresia page img added

--- app/membresias/page.tsx
+++ app/membresias/page.tsx
@@ -39,8 +39,27 @@ export default function Membresias() {
             </section>
 
             {/* Membresias Section */}
+             <section className="text-center bg-gray-50 py-12 px-8 rounded-lg shadow-lg">
+                <h1 className="text-4xl font-bold mb-4 text-navy-blue">MEMBRESIAS</h1>
+                <p className="text-lg mb-6 text-gray-700">
+                    Clientes con membresia tendran un servicio especial y descuentos para su vehículos.
+                </p>
+                <div className="mb-8">
+                    <h2 className="text-2xl font-semibold text-green-600">100% GARANTIZADO</h2>
+                    <h3 className="text-xl font-medium text-gray-600">Mantención Profesional</h3>
+                </div>
+                <div className="relative w-full h-64 md:h-96 rounded-lg overflow-hidden shadow-md">
+                    <Image
+                        src="/membresia/tabea.jpg"
+                        layout="fill"
+                        objectFit="cover"
+                        alt="auto"
+                        className="transition-transform transform hover:scale-105 duration-500"
+                        priority
+                    />
+                </div>
+            </section>
             <section className="mt-16 bg-gray-100 py-12 px-8 rounded-lg shadow-lg text-navy-blue space-y-12">
-                {/* Membership Lite */}
                 <div className="text-center">
                     <h1 className="text-3xl font-bold mb-4">
                         MEMBRESÍA <span className="text-red-600">LITE</span>
@@ -70,7 +89,7 @@ export default function Membresias() {
                         <li>Retiro del vehículo en domicilio o trabajo para llevar al taller y entrega del mismo a domicilio después del arreglo (sin costo).</li>
                         <li>1 scanner y chequeo gratis al año.</li>
                         <li>1 cambio de ampolletas gratis al año.</li>
-                        <li>30% Desc. mantenimiento anual.</li>
+                        <li>30% Desc. mantención anual.</li>
                         <li>30% Desc. en instalaciones (inmovilizadores, cierre centralizado, alarma, radio Android (táctil) y cámara de retroceso).</li>
                         <li>50% Desc. cambio de aceite y filtro al año.</li>
                         <li>Trámites de revisión técnica, permiso de circulación y SOAP gratis (no incluye costo de permiso de circulación).</li>
@@ -91,6 +110,7 @@ export default function Membresias() {
                         <li>1 scanner y chequeo gratis al año.</li>
                         <li>1 cambio de ampolletas gratis al año.</li>
                         <li>50% Desc. mantenimiento anual.</li>
+                        <li>50% Desc. en instalaciones (inmovilizador, cierre centralizado, alarma, radio Android (táctil) y cámara de retroceso.)</li>
                         <li>50% Desc. servicio de grúa.</li>
                         <li>1 cambio de aceite y filtro al año.</li>
                         <li>Trámites de revisión técnica, permiso de circulación y SOAP gratis (no incluye costo de permiso de circulación).</li>
