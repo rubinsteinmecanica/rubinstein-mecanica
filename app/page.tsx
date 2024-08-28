484b3b86e8ea25c01c5bf71be83407cdedc864ca
Christopher Cortes <christopherctes@hotmail.com>
2024-08-27 22:07:27 -0400

    hr added

--- app/page.tsx
+++ app/page.tsx
@@ -37,17 +37,21 @@ export default function Home() {
         <ServiciosPrincipales MainServices={MainServices} />
       </section>
 
+      <hr className="my-8 border-gray-300" />
+
       {/* Carousel Section */}
       <section className="pb-10">
         <Carousel slides={homeBanner} />
       </section>
+
+      <hr className="my-8 border-gray-300" />
 
       {/* Additional Information Section */}
       <section className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8 lg:px-12 mb-12 bg-white shadow-lg rounded-lg">
         <h2 className="text-2xl sm:text-3xl font-semibold text-center mb-6 text-gray-800">
           Atención y Mantención Personalizada
         </h2>
-        <p className="text-center text-base sm:text-lg lg:text-xl leading-relaxed mb-8 text-gray-700">
+        <p className="text-base sm:text-lg lg:text-xl leading-relaxed mb-8 text-gray-700 text-center">
           En Rubinstein nos encargamos de la reparación y mantención de su vehículo, con garantía por los trabajos realizados. Hacemos seguimiento de mantenciones, asesoramiento en mecánica, repuestos, lubricantes, Scanner y accesorios para mejorar la seguridad de su vehículo. Ofrecemos convenios de atención preferencial para empresas y agrupaciones, para mantención de la flota de vehículos corporativos, arreglos en general y trámites de permisos. Además, tenemos un innovador sistema de membresía para clientes particulares.
         </p>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-8">
@@ -62,6 +66,7 @@ export default function Home() {
             <Link
               href="/convenios"
               className="inline-block px-6 py-3 sm:px-8 sm:py-4 md:px-10 md:py-5 lg:px-12 lg:py-6 bg-blue-600 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-700 transition-transform transform hover:scale-105"
+              aria-label="Ver convenios para empresas"
             >
               Empresas
             </Link>
@@ -77,6 +82,7 @@ export default function Home() {
             <Link
               href="/membresias"
               className="inline-block px-6 py-3 sm:px-8 sm:py-4 md:px-10 md:py-5 lg:px-12 lg:py-6 bg-blue-600 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-700 transition-transform transform hover:scale-105"
+              aria-label="Ver membresías para particulares"
             >
               Particulares
             </Link>
@@ -91,7 +97,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
           className="block bg-green-500 rounded-full p-2 shadow-lg"
-          aria-label="Contact Us on WhatsApp"
+          aria-label="Contacta con nosotros en WhatsApp"
         >
           <Image
             src="/whatsapp.png"
